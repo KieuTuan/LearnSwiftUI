73532e2c5be18bd6484667c507a6183a7d2bc2f0
Kieu Anh Tuan <kieutuan25998@gmail.com>
2024-01-07 17:34:11 +0700

    Create project

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Kieu Anh Tuan on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
